406431a4420cd71450aee1678b37e3b7dad0edc9
sadhin28 <taosifsadhin113@gmail.com>
2025-10-23 02:52:12 +0600

    add hover animate zoom ping

--- src/app/page.js
+++ src/app/page.js
@@ -16,7 +16,7 @@ export default function Home() {
   return (
    <div>
       <div className="p-2">
-      <div className="hover:shadow-xl py-10 rounded-xl  grid gap-8 md:grid-cols-12">
+      <div className=" py-10 rounded-xl  grid gap-8 md:grid-cols-12">
         <div className="text-xl  md:text-4xl  font-bold content-center  w-full  col-span-6">
           {/* user TypeWriter */}
           <div className="flex justify-center md:justify-start">
@@ -40,15 +40,15 @@ export default function Home() {
         <div className="text-2xl  font-bold content-center  w-full  col-span-6">
           {/* user image */}
           <div className="flex rounded-full  justify-center md:justify-end">
-            <Image src={protfolioImg} alt="Profile Image" className="hover:rotate-360 transform-3d duration-2000 hover:backdrop-hue-rotate-90 rounded-full w-72 h-72 shadow-2xl hover:shadow-blue-700  border-10 border-white/20 object-cover transition-transform  hover:scale-105" />
+            <Image src={protfolioImg} alt="Profile Image" className=" transform-3d duration-3000 hover:backdrop-hue-rotate-360 rounded-full w-72 h-72 shadow-2xl hover:shadow-blue-700  border-10 border-blue-600/20 hover:border-x-green-500 hover:border-y-red-600 animate-pulse  object-cover  transform-view  hover:scale-105" />
           </div>
 
         </div>
 
       </div>
       <div className="py-5 flex items-center justify-center gap-11">
-        <button onClick={handleClick} className="bg-amber-700 hover:bg-amber-800 rounded-[8px] text-center shadow-2xl shadow-green-800 cursor-pointer border-amber-600 hover:shadow-2xs py-1 px-2 text-xl ">Download Cv</button>
-        <Link href="/contact" className="rounded-[8px] text-center hover:bg-blue-500 shadow-2xl hover:shadow-green-500 cursor-pointer border hover:border-0 py-1 px-2 text-xl ">Hire Me</Link>
+        <button onClick={handleClick} className="rounded-[8px] text-center hover:bg-green-500 font-bold hover:text-black shadow-2xl hover:shadow-green-500 cursor-pointer border hover:border-0 py-1 px-2 text-xl ">Download Cv</button>
+        <Link href="/contact" className="rounded-[8px] text-center hover:bg-blue-500 hover:text-black font-bold shadow-2xl hover:shadow-green-500 cursor-pointer border hover:border-0 py-1 px-2 text-xl ">Hire Me</Link>
 
       </div>
     </div>
